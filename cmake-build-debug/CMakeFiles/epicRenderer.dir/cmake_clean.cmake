47a43d11d102c3ba25d684125b4a7cb0396494f9
Hendrik <hendrik_huebner@yahoo.de>
2021-11-25 22:13:07 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/epicRenderer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epicRenderer.dir/main.cpp.obj"
+  "epicRenderer.exe"
+  "epicRenderer.exe.manifest"
+  "epicRenderer.pdb"
+  "libepicRenderer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/epicRenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
