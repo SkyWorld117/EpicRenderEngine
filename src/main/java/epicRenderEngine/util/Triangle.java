1a134af127bdb8b8f3f72da2d153a36a2db679a0
SkyWorld117 <alanzhu02@gmail.com>
2022-01-13 15:06:40 +0100

    Update Triangle

--- src/main/java/epicRenderEngine/util/Triangle.java
+++ src/main/java/epicRenderEngine/util/Triangle.java
@@ -4,10 +4,14 @@ public class Triangle {
     public Vector3f v0;
     public Vector3f v1;
     public Vector3f v2;
+    public int r, g, b;
 
-    public void Triangle(Vector3f v0, Vector3f v1, Vector3f v2) {
+    public void Triangle(Vector3f v0, Vector3f v1, Vector3f v2, int r, int g, int b) {
         this.v0 = v0;
         this.v1 = v1;
         this.v2 = v2;
+        this.r = r;
+        this.g = g;
+        this.b = b;
     }
 }
