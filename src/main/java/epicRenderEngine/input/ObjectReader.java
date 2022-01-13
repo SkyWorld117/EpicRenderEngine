3b48c937c7b81819a98da7b96fc8d020a648606c
SkyWorld117 <alanzhu02@gmail.com>
2022-01-13 15:04:44 +0100

    ObjectReader

--- src/main/java/epicRenderEngine/input/ObjectReader.java
+++ src/main/java/epicRenderEngine/input/ObjectReader.java
@@ -1,4 +1,40 @@
 package epicRenderEngine.input;
 
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import epicRenderEngine.util.Triangle;
+import epicRenderEngine.util.Vector3f;
+
 public class ObjectReader {
+    public ArrayList<Triangle> load(String str) {
+        Scanner scanner = new Scanner(new File(str));
+        int color = 0;
+        int[] p = new int[3];
+        ArrayList<Triangle> triangles = new ArrayList<Triangle>();
+
+        while (scanner.hasNextInt()) {
+            for (int i = 0; i < 3; i++) {
+                p[i] = scanner.nextInt();
+            }
+            Vector3f v0 = new Vector3f(p[0], p[1], p[2]);
+            for (int i = 0; i < 3; i++) {
+                p[i] = scanner.nextInt();
+            }
+            Vector3f v1 = new Vector3f(p[0], p[1], p[2]);
+            for (int i = 0; i < 3; i++) {
+                p[i] = scanner.nextInt();
+            }
+            Vector3f v2 = new Vector3f(p[0], p[1], p[2]);
+
+            color = scanner.nextInt();
+            int red = (color >> 16) & 0xFF;
+            int green = (color >> 8) & 0xFF;
+            int blue = color & 0xFF;
+
+            triangles.add(new Triangle(v0, v1, v2, red, green, blue));
+        }
+        return triangles;
+    }
 }
