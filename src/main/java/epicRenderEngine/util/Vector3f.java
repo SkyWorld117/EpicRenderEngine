3997f29c8260299fe42ab8d58d627fc4da3b5d0f
jojo-12345 <johanna.lexau@mail.louisenlund.de>
2022-01-17 08:29:48 +0100

    Vector3f Class fertig und kommentiert

--- src/main/java/epicRenderEngine/util/Vector3f.java
+++ src/main/java/epicRenderEngine/util/Vector3f.java
@@ -5,6 +5,7 @@ public class Vector3f {
     float x = 0, y = 0, z = 0;
 
     public Vector3f(float x, float y, float z) {
+        //Constructor
         this.x = x;
         this.y = y;
         this.z = z;
@@ -24,33 +25,40 @@ public class Vector3f {
         return new Vector3f(x_vec, y_vec, z_vec);
     }
 
-    public Vector3f add(Vector3f vecs) {
-        return ZERO;
+    public Vector3f add(Vector3f vec) {
+        //adds two vectors
+        return new Vector3f(this.x + vec.x, this.y + vec.y, this.z + vec.z);
     }
 
-    public Vector3f sub(Vector3f vecs) {
-        return ZERO;
+    public Vector3f sub(Vector3f vec) {
+        //subtracts vector vec
+        return new Vector3f(this.x - vec.x, this.y - vec.y, this.z - vec.z);
     }
 
     public Vector3f scale(float scalar) {
-        return ZERO;
+        //scales vector
+        return new Vector3f(scalar * this.x, scalar * this.y, scalar * this.z);
     }
 
     public void set(float x, float y, float z) {
+        //Set values of the vector
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
     public Vector3f addX(float dx) {
+        //adds dx to x-coordinate
         return new Vector3f(this.x + dx, this.y, this.z);
     }
 
     public Vector3f addY(float dy) {
+        //adds dy to y-coordinate
         return new Vector3f(this.x, this.y + dy, this.z);
     }
 
     public Vector3f normalize() {
+        //normalize the vector to length 1
         float length = (float)Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
         return new Vector3f(this.x / length, this.y / length, this.z / length);
     }
